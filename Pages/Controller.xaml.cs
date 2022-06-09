5574b5facf7a32dfa15cab93f8b3771560680984
Juuso Vastamäki <juuso.vastamaki@tuni.fi>
2022-06-09 13:47:43 +0300

    Added improved bluetooth device finder

--- Pages/Controller.xaml.cs
+++ Pages/Controller.xaml.cs
@@ -1,104 +1,76 @@
 ﻿using System;
-using System.Threading.Tasks;
-using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth.Advertisement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth;
+using Windows.Foundation;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+
+
 
 namespace Farming.Pages
 {
+
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class Controller : Page
     {
 
-
-
         public Controller()
         {
             this.NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
             this.InitializeComponent();
+            Device_List.Items.Add("ASdASd");
+
         }
+        BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
 
-        Windows.UI.Core.CoreDispatcher dispatcher;
-        public static DeviceWatcher deviceWatcher = null;
-        public static int count = 0;
-        public static DeviceInformation[] interfaces = new DeviceInformation[1000];
-        public static bool isEnumerationComplete = false;
-        public static string StopStatus = null;
-        bool BLWatcherStatus = false;
 
+        private bool Is_BT_On = false;
         private void Get_Devices()
         {
             Device_List.Items.Clear();
+            // Checks bluetooth status, if on - updates it
             if (Switch_Bluetooth.IsOn)
             {
-                if (BLWatcherStatus == false)
+                if (Is_BT_On == false)
                 {
-                    try
-                    {
-                        deviceWatcher = DeviceInformation.CreateWatcher();
-                        deviceWatcher.Added += DeviceWatcher_Added;
-                        deviceWatcher.Start();
-                        BLWatcherStatus = true;
-                    }
-                    catch (ArgumentException)
-                    {
-                        Device_List.Items.Clear ();
-                        string Fail = "Try failed!";
-                        Device_List.Items.Add(Fail);
-                        Device_List.Items.Equals(Fail);
-                    }
-
-                    BluetoothUpdateAsync();
+                    Bluetooth_Controller_On();
                 }
+
                 else
                 {
-                    BluetoothUpdateAsync();
+                    Bluetooth_Controller_Update();
                 }
-                // Poista, kun valmis!
-                Device_List.Items.Add("Bluetooth");
+
+                Is_BT_On = true;
             }
-            // Turns off the bluetooth watcher
+
+            // Shuts off Bluetooth
             else
             {
-                if (BLWatcherStatus == true)
+                if (Is_BT_On == true)
                 {
-                    deviceWatcher.Stop();
-                    BLWatcherStatus = false;
+                    Bluetooth_Controller_Off();
                 }
+                Is_BT_On = false;
             }
 
             if (Switch_Wifi.IsOn)
             {
                 Device_List.Items.Add("Wifi");
             }
+
             if (Switch_USB.IsOn)
             {
+
                 Device_List.Items.Add("USB");
             }
         }
-
-        async void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
-        {
-
-        }
-
-        private async Task BluetoothUpdateAsync()
-        {
-            string[] BL_Devices;
-
-            //Using DeviceInformation.FindAllAsync method
-            var deviceInfoCollection = await DeviceInformation.FindAllAsync();
-            foreach (var deviceInfo in deviceInfoCollection)
-            {
-                Device_List.Items.Add(deviceInfo.ToString());
-            }
-
-        }
-
         private void Switch_Bluetooth_Toggled(object sender, RoutedEventArgs e)
         {
             Get_Devices();
@@ -113,5 +85,38 @@ namespace Farming.Pages
         {
             Get_Devices();
         }
+
+        public void Bluetooth_Controller_On()
+        {
+            watcher.Received += OnAdvertisementReceived;
+            //watcher.Start();
+            DevicePicker picker = new DevicePicker(); 
+            picker.Filter.SupportedDeviceSelectors.Add(BluetoothLEDevice.GetDeviceSelectorFromPairingState(false));
+            picker.Filter.SupportedDeviceSelectors.Add(BluetoothLEDevice.GetDeviceSelectorFromPairingState(true));
+
+            picker.Show(new Rect(150, 200, Width, Height));
+        }
+
+        private async void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            DateTimeOffset timestamp = DateTimeOffset.Now;
+            _ = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+              {
+                  Device_List.Items.Add(args.BluetoothAddress);
+              });
+            // Device_List.Items.Add(args.BluetoothAddress);
+        }
+
+        private void Bluetooth_Controller_Update()
+        {
+
+        }
+
+        private void Bluetooth_Controller_Off()
+        {
+
+
+        }
+
     }
 }
